00b28d9d5adcc02094a6b8e7bd70990097508df3
maramraddaoui <63345334+maramraddaoui@users.noreply.github.com>
2022-04-26 00:23:32 +0200

    Update config.py

--- filter/config.py
+++ filter/config.py
@@ -1,1 +1,4 @@
-
+partis={
+    1: ["marine le pen", "marine lepen", "marine", "lepen", "rassemblement national" "mlp", "rn", "le pen"],
+    2: ["macron", "emmanuel", "emmanuel macron","la république en marche", "république en marche","lrem", "larem", "rem", "lrm"]
+}
