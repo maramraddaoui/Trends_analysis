9ea2d017cf8c1249c299cef9f92d55f00cfe2baa
maramraddaoui <63345334+maramraddaoui@users.noreply.github.com>
2022-04-26 00:51:02 +0200

    Update config.py

--- web_scraping/config.py
+++ web_scraping/config.py
@@ -2,6 +2,10 @@ HOST='localhost'
 USER='root'
 PASSWORD=''
 DATABASE='trend'
+
+API_VERSION = "v3"
+DEVELOPER_KEY = "AIzaSyDRMyTjsP3BOeyDrf1qfO4yBn7B-GZRLxs"
+
 SCRIPT_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/script.sh"
 FB_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/facebook.py"
 YT_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/youtube.py"
