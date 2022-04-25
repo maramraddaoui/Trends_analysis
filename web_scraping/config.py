00da462767e49865500874c73e7bc6a97ea65ba6
maramraddaoui <63345334+maramraddaoui@users.noreply.github.com>
2022-04-26 00:28:23 +0200

    Update config.py

--- web_scraping/config.py
+++ web_scraping/config.py
@@ -1,1 +1,8 @@
-
+HOST='localhost'
+USER='root'
+PASSWORD=''
+DATABASE='trend'
+SCRIPT_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/script.sh"
+FB_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/facebook.py"
+YT_FILE_PATH="/home/maram/PFE/Analyse des tendances/web scraping/youtube.py"
+CSV_FILE_PATH="/home/maram/PFE/Analyse des tendances/data.csv"
