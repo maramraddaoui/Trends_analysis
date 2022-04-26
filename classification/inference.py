bfc18c005ba9e44530c8ad16ed0e7994ff603708
maramraddaoui <63345334+maramraddaoui@users.noreply.github.com>
2022-04-26 02:41:19 +0200

    Update inference.py

--- classification/inference.py
+++ classification/inference.py
@@ -8,6 +8,9 @@ def run(comment):
     resultat=model.predict([comment])
     resultat=resultat.tolist()[0]
     i= resultat.index(max(resultat))
-    return i+1
+    if i+1==6 or max(resultat)<0.7:
+        return 6
+    else:
+        return i+1
 if __name__ == "__main__":
     run(sys.argv[1])
